9805ad59fc031f49ce9f8627c69c9ef313a04ee1
luis1997 <luis.lagosamaya1997@gmail.com>
2021-01-23 17:26:26 -0600

    creando RouterLink en las vistas publicas

--- backend/proyecto/src/routes/auth.routes.js
+++ backend/proyecto/src/routes/auth.routes.js
@@ -3,7 +3,7 @@ const router = Router();
 import * as authCtrl from '../controllers/auth.controller'
 import {verifySignup,authJwt} from '../middlewares'
 router.post('/signin',[authJwt.verifyToken,verifySignup.checkRolesExisted],authCtrl.signIn)
-router.post('/signup-user-from-admin',[authJwt.verifyToken,authJwt.isAdmin,verifySignup.checkDuplicateUsernameorEmail],authCtrl.signUp)
+router.post('/signup',[verifySignup.checkDuplicateUsernameorEmail],authCtrl.signUp)
 router.post('/signup-superadmin',verifySignup.checkDuplicateUsernameorEmail,authCtrl.signUp)
 router.delete('/remove-user/:idusuario',[authJwt.verifyToken,authJwt.isSuperAdmin],authCtrl.deleteUserfromSuperAdmin);
 router.put('/change-password',authCtrl.changePassword)
