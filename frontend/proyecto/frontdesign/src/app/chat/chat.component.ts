916a3034880f3895fe56587ecce956e72acb0040
luis1997 <luis.lagosamaya1997@gmail.com>
2021-01-29 21:51:51 -0600

    insertando comentario en la vista de chat

--- frontend/proyecto/frontdesign/src/app/chat/chat.component.ts
+++ frontend/proyecto/frontdesign/src/app/chat/chat.component.ts
@@ -1,6 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Input } from '@angular/core';
 import {DataService} from '../services/data.service';
 import {Observable} from 'rxjs';
+import {FormGroup,FormControl,Validators,FormBuilder} from '@angular/forms'
+import {FormsModule} from '@angular/forms';
+import {NgForm} from '@angular/forms';
+import swat from 'sweetalert2';
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.component.html',
@@ -9,10 +13,41 @@ import {Observable} from 'rxjs';
 export class ChatComponent implements OnInit {
 
   allComments;
+  archivo:any='';
+  comentario;
   constructor(public dataservice:DataService) { }
 
   ngOnInit(): void {
     this.allComments =  this.dataservice.getAllComments();
+  
   }
+  
+  addComment(){
+    if(this.comentario == ''){
+       swat.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: '!No puede postear un comentario en blanco!'
+      });
+    }
+    else
+    {
+      this.dataservice.postComment(this.comentario,this.archivo).subscribe();
+      swat.fire({
+        title: '!Comentario añadido con exito!',
+        showClass: {
+          popup: 'animate__animated animate__fadeInDown'
+        },
+        hideClass: {
+          popup: 'animate__animated animate__fadeOutUp'
+        }
+      });
+      this.comentario = '';
+    }
+ 
+    
+
+
+   }
 
 }
--- frontend/proyecto/frontdesign/src/app/services/data.service.ts
+++ frontend/proyecto/frontdesign/src/app/services/data.service.ts
@@ -1,17 +1,27 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient,HttpHeaders} from '@angular/common/http';
 import {Observable, observable} from 'rxjs';
+import { isToday } from 'date-fns';
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
 
+  user:string;
+  fecha:Date;
   constructor(public httpclient:HttpClient) { }
 
   backendHost:string = 'http://localhost:4000/api/users';
+  cuerpo:any = new HttpHeaders().set('Content-Type','application/json');
 
    getAllComments():Observable<any>{
      return this.httpclient.get(`${this.backendHost}/getchat`);
    }
 
+   postComment(comentario:string,archivo:any){
+     this.user = localStorage.getItem('username');
+     const f = new Date();
+     this.fecha = f;
+     return this.httpclient.post(`${this.backendHost}/addcomment`,{"username":this.user,"comentario":comentario,"archivo":archivo,"fechahora":this.fecha},{headers:this.cuerpo});
+   }
 }
